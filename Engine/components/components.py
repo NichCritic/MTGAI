de8cb508bd0cfe0e9f937792dc213cd93cdcc25a
Nicholas Aelick <n.aelick@gmail.com>
2020-10-31 13:50:21 -0400

    Initial commit

---
+++ Engine/components/components.py
@@ -0,0 +1,25 @@
+class Creature(object):
+
+    def __init__(self, entity_id, power, toughness):
+        self.entity_id = entity_id
+        self.power = power
+        self.toughness = toughness
+
+
+class Spell(object):
+
+    def __init__(self, entity_id):
+        self.entity_id = entity_id
+
+
+class Land(object):
+
+    def __init__(self, entity_id):
+        self.entity_id = entity_id
+
+
+class Owner(object):
+
+    def __init__(self, entity_id, player_id):
+        self.entity_id = entity_id
+        self.player_id = player_id
