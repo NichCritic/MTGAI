7ec148d6b335b7deee15412e6a8665cbf0e7d8e1
Nicholas Aelick <naelick@espial.com>
2020-11-01 21:34:11 -0500

    working on priority loop

--- Engine/driver/driver.py
+++ Engine/driver/driver.py
@@ -1,2 +1,25 @@
-class Driver():
+class Driver(object):
 	pass
+
+
+def priority_loop(game_state, p1, p2):
+	#P1 puts things on the stack until they decline, then P2 puts things on the stack until they decline. If "things" > 0, then the other player gets a chance to put things on the stack
+	#If both players pass resolve the top of the stack then repeat
+
+	#List in reverse order
+	pq = [p2, p1]
+	action = (None, None)
+	while not action == "PASS":
+		p = pq.pop()
+		actions = get_actions(game_state, p)
+		p.send_actions(actions)
+		action = wait_for_action(p)
+		if not action == "PASS":
+			put_action_on_stack()
+		else:
+			pass
+
+
+
+
+
